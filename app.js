479ccb7c99491ea82a1f2f92c58a3b5911705dec
Sabelo710 <sabeloskosana476@gmail.com>
2024-02-19 21:14:11 +0200

    initial commit of basic express.js proxy

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors'); // Import the cors package
+
+var indexRouter = require('./routes/index');
+var projectsRouter = require('./routes/projects');
+
+var app = express();
+
+// Allow requests only from specific origins
+const corsOptions = {
+    origin: ['http://localhost:5173'],
+  };
+
+// Enable CORS for all routes
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/projects', projectsRouter);
+
+module.exports = app;
